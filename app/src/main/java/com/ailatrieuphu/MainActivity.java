905bd651665e03e0bb5daae5189d8d4c1a319809
Bùi Khánh <khanhbuix9@gmail.com>
2020-12-30 00:15:28 +0700

    add activity tutorial

--- app/src/main/java/com/ailatrieuphu/MainActivity.java
+++ app/src/main/java/com/ailatrieuphu/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button bnt_start_now;
+    Button bnt_start_now,cancelEventClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +19,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void bindId (){
+        cancelEventClick = findViewById(R.id.gone_clearEnventClick);
         bnt_start_now = findViewById(R.id.bnt_start_now);
+
+        cancelEventClick.setVisibility(View.GONE);
+
     }
 
     public void onStartNow(View view) {
+        cancelEventClick.setVisibility(View.VISIBLE);
         Intent i = new Intent(this, PlayGameActivity.class);
         startActivity(i);
+        cancelEventClick.setVisibility(View.INVISIBLE);
+    }
+
+    public void onTutorialPlayGame(View view){
+        cancelEventClick.setVisibility(View.VISIBLE);
+        Intent i = new Intent(this, TutorialPlayGames.class);
+        startActivity(i);
+        cancelEventClick.setVisibility(View.INVISIBLE);
     }
 
     public void onContributeQuestions(View view) {
+        cancelEventClick.setVisibility(View.VISIBLE);
         Intent i = new Intent(this, ContributeQuestionsActivity.class);
         startActivity(i);
+        cancelEventClick.setVisibility(View.INVISIBLE);
     }
 }
